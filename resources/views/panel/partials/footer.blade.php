dc3af8ad7eabb6fce06b02c4321c00b97e5234d1
Sina <Sina Sharifzade>
2018-10-12 20:12:28 +0330

    Support publishing views

--- resources/views/panel/partials/footer.blade.php
+++ resources/views/panel/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs"><b>{{ trans('admin.version') }}</b> {{ config('app.version', '0.0.0') }}</div>
-    {!! trans('copyright', ['date' => date('Y'), 'url' => 'https://elegantweb.ir']) !!}
+    {!! trans('admin.copyright', ['date' => date('Y'), 'url' => 'https://elegantweb.ir']) !!}
 </footer>
