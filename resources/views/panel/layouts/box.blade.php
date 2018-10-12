3abb2c84275d2383ae610936b7d5602acf3e8b0f
Sina <Sina Sharifzade>
2018-10-12 19:21:15 +0330

    Adds box layout

---
+++ resources/views/panel/layouts/box.blade.php
@@ -0,0 +1,18 @@
+@extends('admin::panel.layouts.base')
+
+@section('content')
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">@yield('box-title')</h3>
+        <div class="box-tools pull-right">
+            @yield('box-tools')
+        </div>
+    </div>
+    <div class="box-body">
+        @yield('content')
+    </div>
+    <div class="box-footer">
+        @yield('box-footer')
+    </div>
+</div>
+@overwrite
