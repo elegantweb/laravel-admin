dd5a3d118691446af9b4abf06f2cfd1ccfe05e88
Sina Sharifzade <sharifzadesina@gmail.com>
2018-11-07 23:57:36 +0330

    Rename node_modules to components

--- resources/views/panel/layouts/base.blade.php
+++ resources/views/panel/layouts/base.blade.php
@@ -5,23 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/ionicons/dist/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/admin-lte/dist/css/AdminLTE.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/admin/node_modules/admin-lte/dist/css/skins/'.config('admin.panel.skin').'.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/ionicons/dist/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/admin-lte/dist/css/AdminLTE.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/admin/components/admin-lte/dist/css/skins/'.config('admin.panel.skin').'.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/admin/css/admin.css') }}">
     @stack('styles')
 </head>
 <body class="hold-transition {{ config('admin.panel.skin') }} {{ join(config('admin.panel.layout'), ' ') }} @yield('body-class')">
     @include('admin::panel.partials.body')
-    <script src="{{ asset('vendor/admin/node_modules/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/admin/node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('vendor/admin/node_modules/fastclick/lib/fastclick.js') }}"></script>
-    <script src="{{ asset('vendor/admin/node_modules/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('vendor/admin/node_modules/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
-    <script src="{{ asset('vendor/admin/node_modules/admin-lte/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/fastclick/lib/fastclick.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendor/admin/components/admin-lte/dist/js/adminlte.min.js') }}"></script>
     <script src="{{ asset('vendor/admin/js/admin.js') }}"></script>
     @stack('scripts')
 </body>
