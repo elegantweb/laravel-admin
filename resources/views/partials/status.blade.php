b13ede11836c433cf98e586b3fee95a5194c8144
Sina <sharifzadesina@gmail.com>
2020-04-15 23:24:03 +0430

    Support more status alerts

--- resources/views/partials/status.blade.php
+++ resources/views/partials/status.blade.php
@@ -15,3 +15,15 @@
         {{ session('status:failure') }}
     @endcomponent
 @endif
+
+@if (session('status:info'))
+    @component('admin::components.alert', ['varient' => 'info'])
+        {{ session('status:info') }}
+    @endcomponent
+@endif
+
+@if (session('status:warning'))
+    @component('admin::components.alert', ['varient' => 'warning'])
+        {{ session('status:warning') }}
+    @endcomponent
+@endif
