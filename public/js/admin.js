2d376e1127496c88ac0ef5618cd61a17a4b54688
Sina Sharifzade <sharifzadesina@gmail.com>
2020-09-12 14:24:14 +0430

    Update

--- public/js/admin.js
+++ public/js/admin.js
@@ -16,6 +16,14 @@ $.extend(true, $.fn.dataTable.defaults, {
 		 "<'row'<'col-sm-5'i><'col-sm-7'p>>"
 });
 
+$.fn.dataTable.render.intlDateTime = function (options, locale) {
+    const defaultOptions = { dateStyle: "medium", timeStyle: "medium" };
+    const defaultLocale = document.documentElement.lang;
+    return function (data) {
+        return new Intl.DateTimeFormat(locale ?? defaultLocale, { ...defaultOptions, ...options }).format(new Date(data));
+    };
+};
+
 /* Sidebar Menu State */
 
 $('.sidebar-menu a').each(function () {
@@ -30,7 +38,7 @@ $('.sidebar-menu li.treeview > a').each(function () {
     }
 });
 
-/* Select field autovalue */
+/* Select Field Auto Value */
 
 $('select[value]').each(function () {
     $(this).find(`option[value="${$(this).attr('value')}"]`).prop('selected', true);
