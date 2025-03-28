78ded36cf5d7a59e27bc1d43f77e5ae1a62fe818
Sina Sharifzade <sharifzadesina@gmail.com>
2025-03-28 20:37:56 +0000

    Bug Fix

--- resources/views/auth/layouts/base.blade.php
+++ resources/views/auth/layouts/base.blade.php
@@ -1,6 +1,6 @@
 @extends('admin::layouts.base')
 
-@section('body-class', 'login-page bg-body-secondary')
+@push('body-class', ' login-page bg-body-secondary')
 
 @section('content')
     @include('admin::auth.partials.body')
